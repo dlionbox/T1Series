f866271205fb179785ffdd171b7528d074ce9eb7
dlionbox <anascalet2@gmail.com>
2020-07-26 21:10:24 -0300

    Questão 1 feita.

--- t1series.R
+++ t1series.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(forecast)
 library(xts)
 library(imputeTS)
+library(anomalize)
 
 # Lendo os Dados ###############################################################
 
@@ -88,11 +89,67 @@ df2$Week <- cut.Date(df2$V1, breaks = "weeks")
 df_ts <- df2 %>% group_by(Week) %>% summarise(Mortes=sum(V2))
 df_ts$Week <- as.Date(df_ts$Week, format = "%Y-%m-%d")
 
-plot(x = df_ts$Week,
-     y = df_ts$Mortes,
-     type = "l", 
-     main = "Mortes Semanais - Chicago", 
-     xlab = "Data",
-     ylab = "Mortes")
+# Questão 1 ####################################################################
 
+# Transformando o dataframe em série xts
+serie <- xts(df_ts$Mortes, order.by = df_ts$Week, frequency = 52)
 
+# Características empíricas da série
+plot(serie, main = "Chicago - Mortes Semanais")
+plot(decompose(ts(df_ts$Mortes, frequency = 52)))
+
+# Tendência
+# Aparentemente, a série apresenta uma tendência decrescente (não se sabe se
+# linear, quadrática ou outro).
+# Pela decomposição, confirmamos a presença de tendência decrescente 
+# possivelmente linear.
+# Vemos que a partir de 1995, as mortes decrescem mais rapidamente.
+
+# Sazonalidade
+# A série parece ter observações com um comportamento muito parecido ao longo 
+# de semanas/meses Isso é um indício de presença de sazonalidade.
+# Pela decomposição feita, percebemos uma amplitude baixa da sazonalidade em
+# relação à tendência e ao resíduo. Para checarmos a influência da sazonalidade
+# na série, usamos:
+serie <- ts(df_ts$Mortes, frequency = 52)
+dec <- decompose(serie)
+Rt <- dec$random 
+Rt <- Rt[-c(1:26, 706:731)]
+St <- dec$seasonal
+St <- St[-c(1:26, 706:731)]
+Tt <- dec$trend
+Tt <- Tt[-c(1:26, 706:731)]
+
+# De 0 a 1, a sazonalidade possui valor 0.3
+# É considerável...
+FS <- max(0,1-(var(Rt)/var(St+Rt)))
+FS
+
+# A tendência possui um valor maior, de 0.55
+FT <- max(0,1-(var(Rt)/var(Tt+Rt)))
+FT
+
+# Outliers
+# Pelo pacote anomalize, conseguimos detectar anomalias na nossa série,
+# baseados no resíduo de uma decomposição.
+df_ts %>% 
+  time_decompose(Mortes) %>%
+  anomalize(remainder) %>%
+  time_recompose() %>%
+  plot_anomalies(time_recomposed = TRUE, ncol = 3, alpha_dots = 0.5)
+
+# Quem são as anomalias?
+df_ts %>% 
+  time_decompose(Mortes) %>%
+  anomalize(remainder) %>%
+  time_recompose() %>%
+  filter(anomaly == 'Yes')
+
+# A série começa com um ponto aberrante na semana do dia 29/Dez/86. Isso se 
+# deve pelo fato de que nossas observações começam no dia 01/Jan/87 (uma 
+# quarta-feira), logo o número de mortes não leva em consideração domingo, 
+# segunda e terça dessa semana.
+# Para entender o motivo da anomalia do dia 06/Mar/89, precisamos entender
+# melhor de onde vieram nossos dados (coisa que não tenho).
+
+# Questão 2 ####################################################################
